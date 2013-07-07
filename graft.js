666a3d1644435e64b264ffb7a197db6492d22c61
Adrian Rossouw <adrian@daemon.co.za>
2013-07-07 06:47:42 +0200

    added comments to the entry points on the server.

--- graft.js
+++ graft.js
@@ -1,20 +1,44 @@
 var _            = require('underscore');
 var $ = global.$ = require('jquery');
 
+// This is the shared code that actually create the
+// initial Graft object for us.
 Graft = global.Graft = require('./graft.shared');
 
 // Hopefully this will be unecessary one day.
 Graft.server = true;
 
+// Bootstrap module system.
 require('./lib/modules');
+
+// Load the middleware extensions.
 require('./middleware');
 
+// Load up the primary Server middleware. (required)
 require('./middleware/Server.graft.js');
+
+// Load up the REST api middleware. (optional)
+// 
+// Provides /api/$modelName/$id routes for all
+// registered model types, and CRUD.
 require('./middleware/REST.graft.js');
-require('./middleware/Data.test.graft.js');
 
+// Load up the Client middleware (optional)
+//
+// Provides the functionality that bundles up the
+// code base and distributes it to the client.
+require('./middleware/Client.graft.js');
+
+
+// Bind the Server middleware's express route handlers to the
+// Application Object.
+//
+// This allows you to just use Graft.VERB(path, fn) to register
+// routes on the system.
+//
+// We can't just _.extend this, because the start methods
+// on our already extended modules will conflict.
 var Server = Graft.Middleware.Server;
-
 var expressMethods = ['all', 'get', 'post', 'delete', 'use', 'set', 'configure'];
 
 _.each(expressMethods, function(method) {
@@ -31,6 +55,7 @@ function notImplemented() {
 }
 
 // Default data handlers for models.
+Graft.reqres.setHandler('model:name', notImplemented);
 Graft.reqres.setHandler('model:read', notImplemented);
 Graft.reqres.setHandler('model:update', notImplemented);
 Graft.reqres.setHandler('model:delete', notImplemented);
--- index.js
+++ index.js
@@ -1,7 +1,13 @@
+// Initialize the Graft application object.
 var Graft = require('./graft');
 
+// A simple test data adaptor to debug the REST api.
+require('./middleware/Data.test.graft.js');
+
+// Register the index page to be delivered to the client.
 Graft.get('/', function(req, res) {
     res.render('layout', {});
 });
 
+// Start the Application.
 Graft.start();
