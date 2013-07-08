f6e1f328b85b461944ee83e0344a3758e62d5f6f
Adrian Rossouw <adrian@daemon.co.za>
2013-07-08 15:32:13 +0200

    much more complete database api.

--- middleware/Data.test.graft.js
+++ middleware/Data.test.graft.js
@@ -5,7 +5,8 @@ var ns = new (require('Nonsense'))();
 var testData = {} 
 
 testData.Conference = [
-    { id: 'default' }
+    { id: 'default' },
+    { id: 'room2' }
 ];
 
 function fakeCaller(i) {
@@ -15,20 +16,71 @@ function fakeCaller(i) {
     };
 };
 
-testData.Caller = _(_.range(0, 10)).map(fakeCaller);
+function findModel(model, id) {
+    var dfr = new $.Deferred();
+
+    if (!testData[model] || !id) {
+        dfr.reject(405, 'no model or id');
+    } else {
+        var result = _(testData[model]).findWhere({id:id});
+        result ? dfr.resolve(result) : dfr.reject(404);
+    }
+
+    return dfr.promise();
+}
+
+testData.Caller = _(_.range(0, 5)).map(fakeCaller);
 
 _.extend(this, {
     readModel: function readModel(model, id) {
         debug('readModel :', model, id);
+        return findModel(model, id); 
+    },
+    updateModel: function readModel(model, id, data) {
+        debug('updateModel :', model, id);
         var dfr = new $.Deferred();
 
-        if (!testData[model]) {
-            dfr.reject(404);
-        } else {
-            var m = _(testData[model]).findWhere({id:id});
-            m ? dfr.resolve(m) : dfr.reject(404);
+        var data = _.clone(data);
+
+        function updateModel(m) {
+            _.extend(m, data);
+            dfr.resolve(m);
+        }
+        findModel(model, id).then(updateModel, dfr.reject);
+
+        return dfr.promise();
+    },
+    createModel: function(model, data) {
+       debug('createModel :', model, data);
+        var dfr = new $.Deferred();
+
+        var data = _.clone(data);
+
+        function modelExists(model) {
+            dfr.reject(409, 'Conflict');
         }
 
+        // we dont want the model to exist
+        function createModel(err, reason) {
+            _.extend(data, { id: ns.uuid() });
+            testData[model].push(data);
+            dfr.resolve(data);
+        }
+
+        findModel(model, data.id).then(modelExists, createModel);
+
+        return dfr.promise();
+    },
+    deleteModel: function(model, id) {
+        var dfr = new $.Deferred();
+
+        function deleteModel(m) {
+            var ind = _(testData[model]).indexOf(m);
+            testData[model].splice(ind, 1);
+            dfr.resolve(200);
+        }
+
+        findModel(model, id).then(deleteModel, dfr.reject);
         return dfr.promise();
     },
     readCollection: function readModel(col) {
@@ -43,6 +95,9 @@ _.extend(this, {
 this.addInitializer(function(opts) {
     debug("adding handler for reading models");
     Graft.reqres.setHandler('model:read', this.readModel);
+    Graft.reqres.setHandler('model:update', this.updateModel);
+    Graft.reqres.setHandler('model:create', this.createModel);
+    Graft.reqres.setHandler('model:delete', this.deleteModel);
     Graft.reqres.setHandler('collection:read', this.readCollection);
 });
 
--- middleware/REST.graft.js
+++ middleware/REST.graft.js
@@ -2,12 +2,34 @@ var express  = require('express');
 var http     = require('http');
 var path     = require('path');
 
+// This middleware needs to mount routes, so
+// it extends the express object.
+
 var _express = express();
 this.express = _express;
 this.server  = http.createServer(this.express);
 _.extend(this, _express);
 
 
+// Mount all the rest api routes
+this.addInitializer(function(opts) {
+    debug('Initialize REST api');
+    this.post('/api/:model', this.createModel);
+    this.get('/api/:model/:id', this.readModel);
+    this.put('/api/:model/:id', this.updateModel);
+    this.patch('/api/:model/:id', this.updateModel);
+    this.del('/api/:model/:id', this.deleteModel);
+    this.get('/api/:collection', this.readCollection);
+    Graft.reqres.setHandler('model:name', this.modelName);
+    Graft.reqres.setHandler('model:load', this.loadModel);
+});
+
+Graft.Middleware.on('listen', function(Server) {
+    debug('Mounting REST routes');
+    Server.use(this);
+}, this);
+
+// Implementations for each of the methods
 _.extend(this, {
     readModel: function(req, res, next) {
         var send = _.bind(res.send, res);
@@ -20,29 +42,99 @@ _.extend(this, {
             id: req.params.id
         });
 
-
-        debug('model');
-        model.fetch().then(send, send);
+        function fetchModel(attrs) {
+            debug('fetching model', req.params.model, req.params.id);
+            send(model.toJSON()); // it only gets the attributes;
+        }
+        model.fetch().then(fetchModel, send);
     },
     updateModel: function(req, res, next) {
-        var send = _.bind(res.send, res);
-        Graft.request('model:update', req.params.model, req.params.id, req.body)
-            .then(send, send);
+        var send          = _.bind(res.send, res);
+        var hasModelAndId = req.params.model && req.params.id;
+        var modelExists   = !!Graft.models[req.params.model];
+        var options       = req.body;
+
+        if (!hasModelAndId || !modelExists) { return next(404); }
+
+        var model = new Graft.models[req.params.model]({
+            id: req.params.id
+        });
+
+        function saved(attrs) {
+            send(201, model.toJSON());
+        }
+        function saveModel(attrs) {
+            debug('saving model', req.params.model, model.id);
+            model.save(options).then(send, send);
+        }
+
+        model.fetch().then(saveModel, send);
     },
     createModel: function(req, res, next) {
-        var send = _.bind(res.send, res);
-        Graft.request('model:create', req.params.model, req.body)
-            .then(send, send);
+        var hasBody     = req.body;
+        var hasModel    = req.params.model;
+        var modelExists = !!Graft.models[req.params.model];
+
+        if (!hasBody || !hasModel || !modelExists) { 
+            return next(403, 'Forbidden');
+        }
+
+        var send  = _.bind(res.send, res);
+        var model = new Graft.models[req.params.model](req.body);
+
+        function created(attrs) {
+            res.set('Location', model.url());
+            res.send(303, model.toJSON());
+        }
+        function createModel(attrs) {
+            debug('creating model', req.params.model);
+            model.save(options).then(created, send);
+        }
+
+        model.fetch().then(saveModel, send);
     },
     deleteModel: function(req, res, next) {
-        var send = _.bind(res.send, res);
-        Graft.request('model:delete', req.params.model, req.params.id)
-            .then(send, send);
+        var hasModelAndId = req.params.model && req.params.id;
+        var modelExists   = !!Graft.models[req.params.model];
+
+        if (!hasModelAndId || !modelExists) { return next(405); }
+
+        var model = new Graft.models[req.params.model]({
+            id: req.params.id
+        });
+
+        function deleted(attrs) {
+            send(204);
+        }
+
+        function deleteModel(attrs) {
+            debug('deleting model', req.params.model, req.params.id);
+            model.destroy().then(deleted, send);
+        }
+        model.fetch().then(deleteModel, send);
     },
     readCollection: function(req, res, next) {
-        var send = _.bind(res.send, res);
-        Graft.request('collection:read', req.params.collection)
-            .then(send, send);
+        var send             = _.bind(res.send, res);
+        var cName            = _.pluralize(req.params.collection);
+        var collectionExists = !!Graft.models[cName];
+
+        if (!collectionExists) { 
+            return res.send(403, 'Forbidden');
+        }
+
+        var collection   = new Graft.models[cName]();
+        var isCollection = collection instanceof Backbone.Collection;
+
+        if (!isCollection) {
+            return res.send(403, 'Not a collection');
+        }
+
+        // TODO: pass through options?
+        function readCollection(attrs) {
+            debug('read collection', cName);
+            res.send(collection.toJSON());
+        }
+        collection.fetch().then(send, send);
     },
     modelName: function(model) {
         debug('model:name handler');
@@ -77,18 +169,3 @@ function matchUrl(model) {
     return result;
 }
 
-this.addInitializer(function(opts) {
-    debug('Initialize REST api');
-    this.post('/api/:model', this.createModel);
-    this.get('/api/:model/:id', this.readModel);
-    this.put('/api/:model/:id', this.updateModel);
-    this.del('/api/:model/:id', this.deleteModel);
-    this.get('/api/:collection', this.readCollection);
-    Graft.reqres.setHandler('model:name', this.modelName);
-    Graft.reqres.setHandler('model:load', this.loadModel);
-});
-
-Graft.Middleware.on('listen', function(Server) {
-    debug('Mounting REST routes');
-    Server.use(this);
-}, this);
