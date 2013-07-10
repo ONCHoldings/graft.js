6b253511e99106146877d1b556a36eb047063f89
Adrian Rossouw <adrian@daemon.co.za>
2013-07-10 03:37:17 +0200

    moved the mockdb middleware to a different repo.

--- test/fixture/index.js
+++ test/fixture/index.js
@@ -2,15 +2,15 @@
 var Graft = require('../../graft');
 
 // A simple test data adaptor to debug the REST api.
-var Stash = require('./middleware/Stash.graft.js');
+var Mock = require('graft-mockdb');
 require('./models/index.js');
 
 Graft.on('reset:data', function() {
-    Stash.testData.Account = require('./resources/Account.json');
-    Stash.testData.Group = require('./resources/Group.json');
-}, Stash);
+    Mock.testData.Account = require('./resources/Account.json');
+    Mock.testData.Group = require('./resources/Group.json');
+}, Mock);
 
-Stash.on('before:start', function() {
+Mock.on('before:start', function() {
     Graft.trigger('reset:data');
 });
 Graft.start({ port: 8900 });
