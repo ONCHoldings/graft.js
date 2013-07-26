e6de3aefbcbf93ef455dd222dbfb59a313443481
Adrian Rossouw <adrian@daemon.co.za>
2013-07-26 20:47:47 +0200

    write test for cascading static overrides.

--- test/Client.js
+++ test/Client.js
@@ -1,6 +1,8 @@
 var utils    = require('./utils');
 var should   = require('should');
 var testPort = 8902;
+var express  = require('express');
+var connect  = require('connect');
 
 // Initialize the Graft application object.
 var Graft    = require('../server');
@@ -88,6 +90,44 @@ describe('Testing Bundled Routes', function() {
             should.exist(this.body);
         });
     });
+
+    describe('Static provider test', function() {
+        before(function() {
+            Graft.use('/css', express.static(__dirname + '/fixture/css'));
+            Graft.use('/css', express.static(__dirname + '/fixture/assets/css'));
+        });
+        describe('should give me the master css when i request it', function(done) {
+            before(utils.requestUrl(testPort, '/css/master.css'));
+
+            it ('should return status 200', function() {
+                this.resp.should.have.status(200);
+            });
+
+            it ('should have a body', function() {
+                should.exist(this.body);
+            });
+
+            it('should be the right contents', function() {
+                this.body.should.include('MASTER CSS FILE');
+            });
+        });
+        describe('should give me the custom component css when i request it', function(done) {
+            before(utils.requestUrl(testPort, '/css/component.css'));
+
+            it ('should return status 200', function() {
+                this.resp.should.have.status(200);
+            });
+
+            it ('should have a body', function() {
+                should.exist(this.body);
+            });
+
+            it('should be the right contents', function() {
+                this.body.should.include('CUSTOM COMPONENT CSS');
+            });
+        });
+    });
+
 
     describe('stop server', utils.stopServer);
 });
