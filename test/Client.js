1981967d5c51ff220a0cd08629d955c50bbaef30
Adrian Rossouw <adrian@daemon.co.za>
2013-07-26 21:40:06 +0200

    add tests for the locals overriding.

--- test/Client.js
+++ test/Client.js
@@ -1,8 +1,9 @@
 var utils    = require('./utils');
 var should   = require('should');
-var testPort = 8902;
+var path     = require('path');
 var express  = require('express');
 var connect  = require('connect');
+var testPort = 8902;
 
 // Initialize the Graft application object.
 var Graft    = require('../server');
@@ -13,9 +14,37 @@ describe('Testing Bundled Routes', function() {
         // Load up the REST api middleware. (optional)
         require('../middleware/Client.graft.js');
         Graft.load(__dirname + '/fixture');
-        Graft.start({ port: testPort });
-    });
+        var locals = {
+            siteName: "I'M BATMAN",
+            title: "KAPOW"
+        };
 
+        Graft.start({ port: testPort, locals: locals });
+
+        Graft.set('views', path.resolve(__dirname + '/fixture/templates'));
+
+        Graft.get('/', function(req, res, next) {
+            res.render('layout', {});
+        });
+
+    });
+    describe('index page', function() {
+        before(utils.requestUrl(testPort, '/'));
+
+        it ('should return status 200', function() {
+            this.resp.should.have.status(200);
+        });
+        it('response should be html', function() {
+            this.resp.should.have.header('content-type', 'text/html; charset=utf-8');
+        });
+        it ('should have a body', function() {
+            should.exist(this.body);
+        });
+        it('response should have the locals', function() {
+            this.body.should.include('KAPOW');
+            this.body.should.include("I'M BATMAN");
+        });
+    });
     describe('GET /js/templates.js', function() {
         before(utils.requestUrl(testPort, '/js/templates.js'));
 
@@ -30,7 +59,6 @@ describe('Testing Bundled Routes', function() {
             should.exist(this.body);
         });
     });
-
     describe('GET /js/vendor.js', function() {
         before(utils.requestUrl(testPort, '/js/vendor.js'));
 
@@ -127,7 +155,6 @@ describe('Testing Bundled Routes', function() {
             });
         });
     });
-
 
     describe('stop server', utils.stopServer);
 });
