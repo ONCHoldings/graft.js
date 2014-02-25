e82e212a231c4d07a411e956ca8d356fba38a072
Adrian Rossouw <adrian@daemon.co.za>
2014-02-25 06:05:47 +0200

    works even if the sleeping tablets have kicked in!

--- gulpfile.js
+++ gulpfile.js
@@ -2,61 +2,110 @@ var gulp = require('gulp');
 var gutil = require('gulp-util');
 var path = require('path');
 var _ = require('underscore');
+var debug = require('gulp-debug');
 var transform = require('vinyl-transform');
+var source = require('vinyl-source-stream');
 var concat = require('gulp-concat');
+var glob = require('glob');
 
 var Graft = require('./graftfile.js');
-var jadeify2 = require('jadeify2');
 var wrapTransform = require('./lib/wrap.transform');
-var browserify = require('gulp-browserify');
-
-
-function makeRelative(file) {
-    var rel = path.relative(process.cwd(), file);
-    return (/^\./).test(rel) ? rel : './' + rel;
-}
-
-var jadeTransFn = _.wrap(jadeify2, function(fn, file, options) {
-    return fn(file, {
-        client: true,
-        filename: makeRelative(file),
-        compileDebug: false });
-});
+var jade = require('gulp-jade');
+var browserify = require('browserify');
 
 gulp.task('default', function(){
-    var history = [];
+    var vendorBundle = browserify({
+        exposeAll: true
+    });
 
-    gulp.src('./vendor.js', {read: false})
-        .pipe(bundle('vendor'))
+    loadDeps(vendorBundle, Graft.bundles.vendor);
+
+    function loadDeps(bundle, deps) {
+        _(deps).each(reqFn);
+        function reqFn(v) { bundle.require(v); }
+    }
+
+    vendorBundle.bundle()
+        .pipe(source('vendor.js'))
         .pipe(gulp.dest('./build/js'));
 
-    console.log(JSON.stringify(Graft.systems, null, 2));
 
+    var modelBundle = browserify({
+        exposeAll: true,
+        transform: [wrapTransform.through]
+    });
+    modelBundle.external(vendorBundle);
 
+    var dirs = Graft.systems.Model.directories;
+    var files = _(dirs).chain().map(getGlob).flatten().value();
+
+    loadDeps(modelBundle,files);
+
+    console.log(files);
+    function getGlob(d) {
+        var g = makeGlob('js')(d);
+        return glob.sync(g, {cwd: Graft.__graftPath}) ;
+    }
+
+    modelBundle.bundle()
+        .pipe(source('models.js'))
+        .pipe(gulp.dest('./build/js'));
+
+    function loadDirs(dirs) {
+        return function(bundle) {
+            console.trace();
+            var files = _(dirs).map(getGlob);
+            console.log(files);
+        };
+        function getGlob(d) {
+            var g = makeGlob('js')(d);
+            return glob.sync(path.basename(g), { cwd: path.dirname(g) });
+        }
+    }
+
+    function external(extBundle) {
+        return function(bundle) {
+            console.trace();
+            console.log(bundle);
+            bundle.external(extBundle);
+        };
+    }
+/* 
     var sys = Graft.systems.Model;
-    var glob = _(sys.directories).map(function(d) { return d + '/*.js';});
+
+    var glob = _(sys.directories).map(makeGlob('js'));
+
+    var models  = browserify();
+    models.external(vendor);
 
     gulp.src(glob)
         .pipe(transform(wrapTransform.through))
+        .pipe(models)
         .pipe(concat('models.js'))
         .pipe(gulp.dest('./build/js'));
 
-    function bundle(name) {
-        var pipe = browserify()
-            .on('prebundle', prebundle)
-            .on('postbundle', postbundle);
 
-        return pipe;
+    var templates = 
+    var tdirs = Graft.systems.Template.directories;
+    var tglob = _(tdirs).map(makeGlob('jade'));
 
-        function prebundle(bundle) {
-            _(Graft.bundles[name]).each(req);
-            function req(v) {
-                bundle.require(v);
-                history.push(v);
-            }
-        }
-        function postbundle(bundle) {
-            console.log(history);
-        }
+    gulp.src(tglob)
+        .pipe(jade({ client: true }))
+        .pipe(concat('templates.js'))
+        .pipe(gulp.dest('./build/js'));
+
+
+    gulp.src(glob)
+        .pipe(transform(wrapTransform.through))
+        .pipe(bundle('models'))
+        .pipe(concat('models.js'))
+        .pipe(gulp.dest('./build/js'));
+*/
+    function loadDeps(bundle, deps) {
+        _(deps).each(reqFn);
+        function reqFn(v) { bundle.require(v); }
+    }
+    function makeGlob(extension) {
+        return function(d) { return d + '/*.' + extension; };
     }
 });
